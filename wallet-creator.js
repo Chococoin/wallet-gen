1903b033fff98343e8b855e2b2b100b93aa98d27
Germán Lugo <german.lugo@bitnibs.com>
2019-03-19 18:29:46 +0100

    Create batch of wallets.

---
+++ wallet-creator.js
@@ -0,0 +1,28 @@
+const { ethers } = require('ethers');
+const db = require('./database.json');
+const fs = require('fs');
+
+var index = 0
+
+while(index < 100){
+    let randomWallet = ethers.Wallet.createRandom();
+    let obj= {};
+    obj.address = randomWallet.address;
+    obj.mnemonic = randomWallet.mnemonic;
+    obj.privateKay = randomWallet.privateKey;
+    let wal = JSON.stringify(obj);
+    let dir = `./magazine/batch_${(Math.floor((index)/10)+1)}`;
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+        console.log(`Batch ${dir} created.`)
+    }
+    try {
+        fs.writeFileSync(`${dir}/wallet_${index+1}.json`, wal, { flag: "wx" }, (err)=>{
+            console.log(err);
+        });
+        console.log(`File "${index+1}.json" created`)
+    } catch {
+        console.log("File exists");
+    }
+    index++
+}  
