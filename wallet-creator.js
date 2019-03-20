4413304677e3b18a1c1b0cf2f30a3716a56c798a
German Lugo <german.lugo@bitnibs.com>
2019-03-20 01:04:25 +0100

    Create image of QR-code.

--- wallet-creator.js
+++ wallet-creator.js
@@ -10,7 +10,7 @@ while(index < 100){
     let obj= {};
     obj.address = randomWallet.address;
     obj.mnemonic = randomWallet.mnemonic;
-    obj.privateKay = randomWallet.privateKey;
+    obj.privateKey = randomWallet.privateKey;
     let wal = JSON.stringify(obj);
     let dir = `./magazine/batch_${(Math.floor((index)/10)+1)}`;
     if (!fs.existsSync(dir)){
@@ -18,12 +18,17 @@ while(index < 100){
         console.log(`Batch ${dir} created.`);
     }
     try {
-        fs.writeFileSync(`${dir}/wallet_${index+1}.json`, wal, { flag: "wx" }, (err)=>{
+        fs.mkdirSync(`${dir}/${index+1}`);
+        fs.writeFileSync(`${dir}/${index+1}/wallet_${index+1}.json`, wal, { flag: "wx" }, (err)=>{
             console.log(err);
         });
+        let pubkey = qr.image(obj.address, { type: 'png' }, size=10);
+        pubkey.pipe(fs.createWriteStream(`${dir}/${index+1}/Address.png`));
+        let prvkey = qr.image(obj.privateKey, { type: 'png' });
+        prvkey.pipe(fs.createWriteStream(`${dir}/${index+1}/prvkey.png`));
         console.log(`File "${index+1}.json" created`);
     } catch(err) {
-        console.log("File exists");
+        console.log("File exists", err);
     }
-    index++
+    index++;
 }  
